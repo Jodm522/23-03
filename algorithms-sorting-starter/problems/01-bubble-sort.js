1210111ce0a31044b98a449610052688c6749fb2
james <jodm522@gmail.com>
2021-03-23 12:21:29 -0500

    finished problem 1

--- algorithms-sorting-starter/problems/01-bubble-sort.js
+++ algorithms-sorting-starter/problems/01-bubble-sort.js
@@ -7,10 +7,19 @@ function swap(array, idx1, idx2) {
 }
 
 function bubbleSort(array) {
-  // your code here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1 - i; j++) {
+      let index1 = j;
+      let index2 = j + 1;
+      if (array[index1] >= array[index2]) {
+        swap(array, index1, index2);
+      }
+    }
+  }
+  return array;
 }
 
 module.exports = {
   bubbleSort,
-  swap
+  swap,
 };
