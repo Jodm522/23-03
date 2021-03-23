8911ac56124b866148cf12ab54c924f2cebe885f
james <jodm522@gmail.com>
2021-03-23 16:05:15 -0500

    completed all problems

--- algorithms-sorting-starter/problems/05-quick-sort.js
+++ algorithms-sorting-starter/problems/05-quick-sort.js
@@ -2,9 +2,27 @@
 
 function quickSort(array) {
   // your code here
+  if (array.length <= 1) {
+    return array;
+  }
+
+  let pivot = array[0];
+  let leftArray = [];
+  let rightArray = [];
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] <= pivot) {
+      leftArray.push(array[i]);
+    } else if (array[i] > pivot) {
+      rightArray.push(array[i]);
+    }
+  }
+
+  let sortedLeftArray = quickSort(leftArray);
+  let sortedRightArray = quickSort(rightArray);
+  return sortedLeftArray.concat(pivot).concat(sortedRightArray);
 }
 
-
 module.exports = {
-  quickSort
+  quickSort,
 };
