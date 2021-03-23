633b6bdd494e6e2207bb765e2003392ae349c839
Jia Cui <cuijiajanae@gmail.com>
2021-03-23 10:35:31 -0700

    finished problem 2

--- algorithms-sorting-starter/problems/02-selection-sort.js
+++ algorithms-sorting-starter/problems/02-selection-sort.js
@@ -3,10 +3,23 @@
 // Implement swap without looking at bubble sort
 function swap(arr, index1, index2) {
   // your code here
+  let temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
 }
 
 function selectionSort(list) {
   // your code here
+  for(let i = 0; i < list.length; i++){
+    let smallest = i;
+    for(let j = i + 1; j < list.length; j++){
+      if(list[smallest] > list[j]){
+        smallest = j;
+      }
+    }
+    swap(list, i, smallest)
+  }
+  return list;
 }
 
 module.exports = {
